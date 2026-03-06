0e91a94ab6eba074a0c1ccde95f03ad0557a4b00
ruben <ruben.gallardo@inslapineda.cat>
2026-03-06 13:41:42 +0100

    Added en el main_scene script para manejar en un array la scene_actual y el siguiente nivel

---
+++ src/stages/main_scene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var niveles: Array[PackedScene]
+
+var _nivel_actual: int = 2
+var _nivel_instanciado: Node
+
+
+func _ready() -> void:
+	crear_nivel(_nivel_actual)
+
+func crear_nivel(numero_nivel: int):
+	_nivel_instanciado = niveles[numero_nivel - 1].instantiate()
+	add_child.call_deferred(_nivel_instanciado)
+
+func eliminar_nivel():
+	_nivel_instanciado.queue_free()
+	
+func reinicar_nivel():
+	eliminar_nivel()
+	crear_nivel.call_deferred(_nivel_actual)
