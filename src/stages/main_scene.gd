d7ab9b2ab599d4473729b4d67ccf1cb67d2874f7
ruben <ruben.gallardo@inslapineda.cat>
2026-03-20 15:15:47 +0100

    Funcionalidad al tocar la tecla N pase al siguiente nivel del juego. Es un CHEAT

--- src/stages/main_scene.gd
+++ src/stages/main_scene.gd
@@ -9,6 +9,20 @@ var _nivel_instanciado: Node
 func _ready() -> void:
 	crear_nivel(_nivel_actual)
 
+func _input(event: InputEvent) -> void:
+	# Cheat: Saltar al siguiente nivel con la tecla 'n'
+	if event is InputEventKey and event.pressed and event.keycode == KEY_N:
+		_saltar_nivel()
+
+func _saltar_nivel() -> void:
+	# Verificar que no esté en el último nivel
+	if _nivel_actual < niveles.size():
+		_nivel_actual += 1
+		crear_nivel.call_deferred(_nivel_actual)
+		print("CHEAT: Saltando al nivel ", _nivel_actual)
+	else:
+		print("CHEAT: Ya estás en el último nivel (", _nivel_actual, ")")
+
 func crear_nivel(numero_nivel: int):
 	# Eliminar el nivel anterior si existe
 	if _nivel_instanciado != null:
