c860a7883908c99f5be6bf20087bb6ca714db47b
ruben <ruben.gallardo@inslapineda.cat>
2026-04-10 12:22:55 +0200

    Added sound boots

--- src/items/boots.gd
+++ src/items/boots.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $Collision
+@onready var audio_boots: AudioStreamPlayer = $AudioBoots_doblejump
 
 func _ready():
 	body_entered.connect(_on_body_entered)
@@ -13,5 +14,7 @@ func _on_body_entered(body: Node2D) -> void:
 		# Hide and disable collision
 		sprite.visible = false
 		collision_shape_2d.set_deferred("disabled", true)
-		# Queue free after a short delay or immediately
-		queue_free()
+		# Play Sound
+		audio_boots.play()
+		# Queue free after audio finishes
+		audio_boots.finished.connect(queue_free)
